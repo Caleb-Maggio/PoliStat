74cbb915028443bb4c381bb2419055882548c05f
projectgild <137822248+projectgild@users.noreply.github.com>
2024-12-16 03:47:47 -0500

    modified menubar, added more avatars

--- scripts/global.js
+++ scripts/global.js
@@ -5,8 +5,10 @@ $("#mobile-menubar-button").click(function() {
     if (!menuDropdown) {
         $("#mobile-menubar-links").show();
         menuDropdown = true;
+        $("#mobile-menubar-button").html("Close Menu");
     } else {
         $("#mobile-menubar-links").hide();
         menuDropdown = false;
+        $("#mobile-menubar-button").html("Open Menu");
     }
 });
--- scripts/make-a-candidate.js
+++ scripts/make-a-candidate.js
@@ -1,13 +1,28 @@
 $("#avatar").on("change", function() {
     switch($(this).val()) {
+        case "none":
+            $("#candidate-icon").attr("src", "images/blank_avatar.png");
+            break;
         case "joe-biden":
             $("#candidate-icon").attr("src", "images/Vector_Biden_Cropped.png");
+            break;
+        case "kamala-harris":
+            $("#candidate-icon").attr("src", "images/Vector_Kamala_Cropped.png");
             break;
         case "barack-obama":
             $("#candidate-icon").attr("src", "images/Vector_Obama_Cropped.png");
             break;
+        case "donald-trump":
+            $("#candidate-icon").attr("src", "images/Vector_Trump_Cropped.png");
+            break;
         case "jd-vance":
             $("#candidate-icon").attr("src", "images/Vector_Vance_Cropped.png");
+            break;
+        case "jimmy-carter":
+            $("#candidate-icon").attr("src", "images/Vector_Carter_Cropped.png");
+            break;
+        case "alex-jones":
+            $("#candidate-icon").attr("src", "images/Vector_Jones_Cropped.png");
             break;
     }
 });
